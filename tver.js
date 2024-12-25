0a22a7d6e4f91bccbfe28f0e2e6ed555864c6fd2
りょう <143770907+VEDA00133912@users.noreply.github.com>
2024-12-25 14:35:02 +0900

    Update tver.js

--- tver.js
+++ tver.js
@@ -1,9 +1,10 @@
+const velocity = 100;
+
 function adAccelerate() {
     const controller = [...document.querySelectorAll('div')]
         .find(div => div.className.includes('controller_container__'));
-    if (!controller) return console.error('controller_containerが見つかりません');
+    if (!controller) return console.error('× controller_containerが見つかりません');
 
-    const velocity = 100;
     const speed = controller.style.getPropertyValue('--seek-bar-color') === '#ffd647' ? 16 : 1;
     document.querySelectorAll('video').forEach(video => {
         if (video.playbackRate !== speed) {
