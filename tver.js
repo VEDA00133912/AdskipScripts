82bfe861068c36f8679d41f93b26691cdffe51c5
りょう <143770907+VEDA00133912@users.noreply.github.com>
2025-03-24 16:05:55 +0900

    Update tver.js

--- tver.js
+++ tver.js
@@ -1,23 +1,17 @@
-let lastTime = null;
-
-function adSetStartPosition() {
-    const controller = [...document.querySelectorAll('div')].find(div => div.className.includes('controller_container__'));
-    if (!controller) return;
-
-    const seekBarColor = controller.style.getPropertyValue('--seek-bar-color');
-    
-    document.querySelectorAll('video').forEach(video => {
-        if (seekBarColor === '#ffd647' && video.currentTime !== 999) {
-            lastTime = video.currentTime;
-            video.currentTime = 999;
-            console.log(`開始位置を999に変更しました`);
-        } else if (lastTime !== null && video.currentTime !== lastTime) {
-            video.currentTime = lastTime;
-            console.log(`開始位置を記録された秒数(${lastTime})に戻しました`);
-        }
-    });
-
-    if (seekBarColor !== '#ffd647') lastTime = null;
-}
-
-new MutationObserver(adSetStartPosition).observe(document.body, { childList: true, subtree: true, attributes: true });
+const velocity = 100;
+ 
+ function adAccelerate() {
+     const controller = [...document.querySelectorAll('div')]
+         .find(div => div.className.includes('controller_container__'));
+     if (!controller) return console.error('× controller_containerが見つかりません');
+ 
+     const speed = controller.style.getPropertyValue('--seek-bar-color') === '#ffd647' ? 16 : 1;
+     document.querySelectorAll('video').forEach(video => {
+         if (video.playbackRate !== speed) {
+             video.playbackRate = speed;
+             console.log(`${speed}xに変更しました`);
+         }
+     });
+ }
+ 
+ setInterval(adAccelerate, velocity);
