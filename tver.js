c9da6cef10dce8bd0deaa7be1abc2c3dfd3460f4
りょう <143770907+VEDA00133912@users.noreply.github.com>
2025-09-28 10:16:34 +0900

    Tverの変更に伴って修正

--- tver.js
+++ tver.js
@@ -1,17 +1,17 @@
 const velocity = 100;
- 
- function adAccelerate() {
-     const controller = [...document.querySelectorAll('div')]
-         .find(div => div.className.includes('controller_container__'));
-     if (!controller) return console.error('× controller_containerが見つかりません');
- 
-     const speed = controller.style.getPropertyValue('--seek-bar-color') === '#ffd647' ? 16 : 1;
-     document.querySelectorAll('video').forEach(video => {
-         if (video.playbackRate !== speed) {
-             video.playbackRate = speed;
-             console.log(`${speed}xに変更しました`);
-         }
-     });
- }
- 
- setInterval(adAccelerate, velocity);
+
+function adAccelerate() {
+    const controller = [...document.querySelectorAll('div')]
+        .find(div => div.className.includes('VodController_container__'));
+    if (!controller) return console.error('× VodController_containerが見つかりません');
+
+    const speed = controller.style.getPropertyValue('--seek-bar-color') === '#ffd647' ? 16 : 1;
+    document.querySelectorAll('video').forEach(video => {
+        if (video.playbackRate !== speed) {
+            video.playbackRate = speed;
+            console.log(`${speed}xに変更しました`);
+        }
+    });
+}
+
+setInterval(adAccelerate, velocity);
